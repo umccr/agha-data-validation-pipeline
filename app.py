4acd7a9a150ee3434c32ead2a95ee38b5866da54
Stephen Watts <hello@stephen.ac>
2021-07-22 11:09:47 +1000

    Convert named arguments to positional

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ ssm_client = boto3.client('ssm')
 def get_ssm_parameter_value(name):
     return ssm_client.get_parameter(Name=name)['Parameter']['Value']
 
-staging_bucket = get_ssm_parameter_value(name='/cdk/agha/staging_bucket')
-store_bucket = get_ssm_parameter_value(name='/cdk/agha/store_bucket')
-slack_host = get_ssm_parameter_value(name='/slack/webhook/id')
-slack_channel = get_ssm_parameter_value(name='/cdk/agha/slack_channel')
-manager_email = get_ssm_parameter_value(name='/cdk/agha/manager_email')
-sender_email = get_ssm_parameter_value(name='/cdk/agha/sender_email')
+staging_bucket = get_ssm_parameter_value('/cdk/agha/staging_bucket')
+store_bucket = get_ssm_parameter_value('/cdk/agha/store_bucket')
+slack_host = get_ssm_parameter_value('/slack/webhook/id')
+slack_channel = get_ssm_parameter_value('/cdk/agha/slack_channel')
+manager_email = get_ssm_parameter_value('/cdk/agha/manager_email')
+sender_email = get_ssm_parameter_value('/cdk/agha/sender_email')
 
 # retrieve AWS details from currently active AWS profile/credentials
 aws_env = {
