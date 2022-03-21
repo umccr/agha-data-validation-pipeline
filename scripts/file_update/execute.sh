b3cce91e7de43470a2ee2dc5f12851ce7d1593c1
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-03-21 16:44:40 +1100

    Update execute.sh

--- scripts/file_update/execute.sh
+++ scripts/file_update/execute.sh
@@ -7,12 +7,12 @@ export BUCKET_LOCATION="agha-gdr-store-2.0"
 export PARTITION_KEY="TYPE:MANIFEST"
 export TARGET_SORT_KEY="ABC/0123456789/SOME-FILE.vcf.gz"
 export SOURCE_SORT_KEY="XYZ/987654321/SOME-FILE.vcf.gz"
-export OVERRIDE_VALUE='{"agha_study_id":"abcde"}'  # Make sure it is a JSON string object
+export OVERRIDE_VALUE='{"agha_study_id":"abcde"}' # Make sure it is a JSON string object
 
 # Update manifest.txt file in submission level by toggling 'UPDATE_MANIFEST_TXT' variable
 # Ideally you would only trigger this for the last move file in the submission
 
-export UPDATE_MANIFEST_TXT=true  # Boolean value (lower case): true, false
+export UPDATE_MANIFEST_TXT=false # Boolean value (lower case): true, false
 
 ######################################################################################################
 
@@ -50,15 +50,19 @@ python move_manifest_record.py \
   --new_sort_key ${TARGET_SORT_KEY} \
   --value_override ${OVERRIDE_VALUE} \
   2>&1 || {
-    echo >&2 "FAIL DYNAMODB UPDATE. ABORTING..."
-    exit 1
+  echo >&2 "FAIL DYNAMODB UPDATE. ABORTING..."
+  exit 1
 }
 
 # Move the file object
-echo "Moving original file"
-s3_uri_source="s3://${BUCKET_LOCATION}/${SOURCE_SORT_KEY}"
-s3_uri_target="s3://${BUCKET_LOCATION}/${TARGET_SORT_KEY}"
-aws s3 mv "${s3_uri_source}" "${s3_uri_target}"
+if [ $SOURCE_SORT_KEY != $TARGET_SORT_KEY ]; then
+  echo "Moving Files to a different location"
+  s3_uri_source="s3://${BUCKET_LOCATION}/${SOURCE_SORT_KEY}"
+  s3_uri_target="s3://${BUCKET_LOCATION}/${TARGET_SORT_KEY}"
+  aws s3 mv "${s3_uri_source}" "${s3_uri_target}"
+else
+  echo "Source and target destination are the same, skipping file move"
+fi
 
 # Update manifest.txt file for each submission
 if [ "$UPDATE_MANIFEST_TXT" == true ] && [ "$BUCKET_LOCATION" == $STORE_BUCKET ]; then
@@ -69,15 +73,17 @@ if [ "$UPDATE_MANIFEST_TXT" == true ] && [ "$BUCKET_LOCATION" == $STORE_BUCKET ]
 fi
 
 # Move '__results.json' location
-echo "Moving results json file with modification"
-python move_and_modify_results_file.py \
-  --source_s3_key "${SOURCE_SORT_KEY}" \
-  --target_s3_key "${TARGET_SORT_KEY}" \
-  2>&1 || {
+if [ $SOURCE_SORT_KEY != $TARGET_SORT_KEY ]; then
+  echo "Moving results json file with modification"
+  python move_and_modify_results_file.py \
+    --source_s3_key "${SOURCE_SORT_KEY}" \
+    --target_s3_key "${TARGET_SORT_KEY}" \
+    2>&1 || {
     echo >&2 "No results file found. Terminating ... "
     exit 1
-}
+  }
+else
+  echo "Source and target destination are the same, skipping result file move"
+fi
 
 echo "Script Execute Successfully"
-
-
