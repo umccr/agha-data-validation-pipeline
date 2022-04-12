7a299f7bbea224c3c906a553471c0ace25dd8319
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-04-12 13:25:55 +1000

    remove `unknown` regex exceptions

--- lambdas/layers/util/util/submission_data.py
+++ lambdas/layers/util/util/submission_data.py
@@ -14,7 +14,7 @@ from util import notification, s3, agha
 
 MANIFEST_REQUIRED_COLUMNS = {'filename', 'checksum', 'agha_study_id'}
 # Manifest field validation related
-AGHA_ID_RE = re.compile('^A[0-9]{7,8}(?:_[a-zA-Z0-9]+)?$|^unknown$')
+AGHA_ID_RE = re.compile('^A[0-9]{7,8}(?:_[a-zA-Z0-9]+)?$')
 MD5_RE = re.compile('^[0-9a-f]{32}$')
 
 # Logging
