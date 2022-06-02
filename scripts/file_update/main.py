53f10c0bd3dd2d996b5e1acc5b9c797d15583f41
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-06-02 17:13:58 +1000

    Double Quote Shell expansion variable

--- scripts/file_update/main.py
+++ scripts/file_update/main.py
@@ -9,11 +9,15 @@ STORE_BUCKET = 'agha-gdr-store-2.0'
 DYNAMODB_STORE_TABLE_NAME = 'agha-gdr-store-bucket'
 
 
+
 def execute_change():
     filter_expr = Attr('agha_study_id').eq('XXXXXX')
+    SUBMISSION_PREFIX = "Flagship/Submission/"
+
     list_of_files = dynamodb.get_batch_item_from_pk_and_sk(DYNAMODB_STORE_TABLE_NAME,
                                                            dynamodb.FileRecordPartitionKey.MANIFEST_FILE_RECORD.value,
-                                                           "Flagship/Submission/", filter_expr=filter_expr)
+                                                           SUBMISSION_PREFIX,
+                                                           filter_expr=filter_expr)
     key_of_list = [metadata['sort_key'] for metadata in list_of_files]
     print(json.dumps(list_of_files, indent=4))
     print(len(list_of_files))
@@ -26,7 +30,7 @@ def execute_change():
                   f"{dynamodb.FileRecordPartitionKey.MANIFEST_FILE_RECORD.value} "
                   f"{key} "
                   f"{new_key} "
-                  f"""'' """
+                  f"""'{json.dumps({'agha_study_id': 'abcde'})}' """  # Put empty object for no changes. Example: '{}'
                   f"true")
         time.sleep(1)  # Allow for time buffering
 
